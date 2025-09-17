f7a6be0e5c95421059d6ce9e17617ebd4511b8fc
aditi7268 <99127320+aditigupta7268@users.noreply.github.com>
2025-09-17 11:52:40 +0530

    Add files via upload

--- SQL28.sql
+++ SQL28.sql
@@ -18,3 +18,44 @@ drop index test_idx on test;
 create index test_idx on test(name,id);
 explain select * from test where name = "ujjwal";
 explain select * from test where id = 18 and name = "ujjwal";
+
+use regex1;
+CREATE TABLE SampleTable (
+    id INT,
+    name VARCHAR(50),
+    age INT,
+    city VARCHAR(50)
+);
+
+INSERT INTO SampleTable (id, name, age, city) VALUES
+(1, 'Akash', 25, 'Jaipur'),
+(2, 'Priya', 24, 'Delhi'),
+(3, 'Rahul', 26, 'Mumbai'),
+(4, 'Sneha', 23, 'Bangalore');
+
+CREATE UNIQUE INDEX name_idx ON SampleTable(name);
+
+explain SELECT * from SampleTable where name = 'Sneha';
+
+
+-- 2.
+DROP TABLE SampleTable;
+CREATE TABLE SampleTable (
+    id INT primary key,
+    name VARCHAR(50) unique,
+    age INT,
+    city VARCHAR(50)
+);
+INSERT INTO SampleTable (id, name, age, city) VALUES
+(1, 'Akash', 25, 'Jaipur'),
+(2, 'Priya', 24, 'Delhi'),
+(3, 'Rahul', 26, 'Mumbai'),
+(4, 'Sneha', 23, 'Bangalore');
+
+SHOW indexes FROM SampleTable;
+
+CREATE INDEX age_idx ON SampleTable(age) INVISIBLE;
+SHOW INDEXES FROM SampleTable;
+
+EXPLAIN SELECT * FROM SampleTable WHERE age = 25;
+
---
+++ SQL29.sql
@@ -0,0 +1,22 @@
+-- PL SQL (procedural SQL)
+# gives the feature of programming + sql 
+# it is a extension for SQL which combines the capability of programming language and capabilities of SQL 
+## it helps in efficiency
+## Blocks in PL SQL :
+## 1. Declaration Block --> to create any variable , function , cursor (optional block)
+## Syntax ---> Declare
+## 2. Executable Block ---> compulsory block , where we write down the logic and SQL statement
+## Syntax ---> Begin  end
+## 3. Exception Block ---> to handle unwanted conditions 
+## Syntax ---> except
+
+## limited features on MySQL so we will use Oracle for PL SQL (community edition)
+## Oracle (enterprise edition)
+
+## Features of PL SQL:
+## 1.Integrated with SQL.
+## 2. Gives the feature to work row by row on a data.
+## 3. Reusability of code 
+## 4. Portable --> caan be executed on other system also
+## 5. Help in developing Web Application.
+## Also known as Server side 
